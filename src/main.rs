78e30a0250a6e1c1cf9eaeb625535e35deb0a0e9
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-11-07 20:33:31 +0900

    add comments to the source code

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,17 @@ struct Args {
     output: String,
 }
 
+// XXX: Currently assumes that the input JSON is a list of objects with `ancestor` and `descendant` keys.
 #[derive(serde::Deserialize)]
 struct ClosureRecord {
     ancestor: String,
     descendant: String,
 }
 
+// Retrieves the root node of the tree.
+// The root node is the node that is an ancestor of all other nodes, but has no ancestor itself.
+// This is done by counting the number of times a node appears as a descendant.
+// If a node appears as a descendant only once, it is the root node.
 fn retrieve_root_node(json: Vec<serde_json::Value>) -> String {
     let mut ancestors: Vec<String> = Vec::new();
     let mut descendants: Vec<String> = Vec::new();
@@ -55,19 +60,22 @@ fn retrieve_root_node(json: Vec<serde_json::Value>) -> String {
         }
     }
 
+    // The program assumes that the root node is always present, if it is not that means it is some cyclic graph, which is not assumed.
     panic!("Root node not found")
 }
 
+// Recursively generates the WBS string from the JSON. The level is used to determine the number of asterisks to prepend to the node (used for printing the PlantUML WBS).
 fn closure_children_to_wbs_string(
     json: Vec<serde_json::Value>,
     current: String,
     level: i32,
 ) -> String {
-    let mut tempstr;
-    tempstr = format!("{} {}\n", "*".repeat(level as usize), current);
+    let mut wbsString;
+    wbsString = format!("{} {}\n", "*".repeat(level as usize), current);
 
     let mut children: Vec<String> = Vec::new();
 
+    // Find all direct children of the current node.
     for record in json.clone() {
         let closure_record: ClosureRecord =
             serde_json::from_value(record).expect("Unable to deserialize");
@@ -76,21 +84,26 @@ fn closure_children_to_wbs_string(
         }
     }
 
+    // Recursively call the function for each direct child.
     for child in children {
-        tempstr.push_str(&closure_children_to_wbs_string(
+        wbsString.push_str(&closure_children_to_wbs_string(
             json.clone(),
             child,
             level + 1,
         ));
     }
 
-    tempstr.to_string()
+    wbsString.to_string()
 }
 
+// Converts the JSON to a PlantUML WBS string.
 fn json_to_plantuml_wbs(json: Vec<serde_json::Value>) -> String {
     let mut plantuml = String::from("@startwbs\n");
+    
+    // Starting from the root node, recursively generate the WBS string.
     let root_node = retrieve_root_node(json.clone());
     plantuml.push_str(&closure_children_to_wbs_string(json.clone(), root_node, 1));
+    
     plantuml.push_str("@endwbs\n");
     plantuml
 }
