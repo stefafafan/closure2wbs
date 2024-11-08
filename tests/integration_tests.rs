7e0bf2d374508445957be5288b063859e0a6275e
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-11-08 18:13:55 +0900

    add simple integration tests for success cases

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,10 +1,48 @@
 use assert_cmd::Command;
+use std::fs;
 
 #[test]
-fn version() {
+fn plantuml() {
     let mut cmd = Command::cargo_bin("closure2wbs").unwrap();
-    cmd.arg("-V")
+    cmd.arg("--input")
+        .arg("tests/fixtures/input.json")
+        .arg("--output")
+        .arg("tests/fixtures/output.puml")
+        .arg("--format")
+        .arg("plantuml")
         .assert()
-        .success()
-        .stdout("closure2wbs 0.3.0\n");
+        .success();
+
+    let output_contents =
+        fs::read_to_string("tests/fixtures/output.puml").expect("Unable to read output file");
+
+    let expected_output =
+        "@startwbs\n* A\n** B\n*** C\n**** F\n**** G\n*** D\n**** H\n*** E\n**** I\n@endwbs\n";
+
+    assert_eq!(output_contents, expected_output);
+
+    fs::remove_file("tests/fixtures/output.puml").expect("Unable to remove output file");
+}
+
+#[test]
+fn mermaid() {
+    let mut cmd = Command::cargo_bin("closure2wbs").unwrap();
+    cmd.arg("--input")
+        .arg("tests/fixtures/input.json")
+        .arg("--output")
+        .arg("tests/fixtures/output.mmd")
+        .arg("--format")
+        .arg("mermaid")
+        .assert()
+        .success();
+
+    let output_contents =
+        fs::read_to_string("tests/fixtures/output.mmd").expect("Unable to read output file");
+
+    let expected_output =
+        "flowchart TD\nA --> B\nB --> C\nC --> F\nC --> G\nB --> D\nD --> H\nB --> E\nE --> I\n";
+
+    assert_eq!(output_contents, expected_output);
+
+    fs::remove_file("tests/fixtures/output.mmd").expect("Unable to remove output file");
 }
